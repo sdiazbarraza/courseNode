fd577484b5188796da15464d84590f8de25321f2
simp <simpdev07@gmail.com>
2017-10-16 20:37:17 -0300

    Add GET /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -30,6 +30,14 @@ app.get('/todos',(req,res)=>{
 });
 app.get('/todos/:id',(req,res)=>{
 	var id = req.params.id;
+	Todo.findById(id).then((todo)=>{
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e)=>{
+		res.status(400).send();
+	});
 	/*
 	Todo.find().then((todos)=>{
 		res.send({todos});
